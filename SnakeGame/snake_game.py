43e7da6699cc5022fa6c7c044a2b551f5013a295
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2021-08-22 23:40:01 +0200

    Add handling of several key presses

--- SnakeGame/snake_game.py
+++ SnakeGame/snake_game.py
@@ -1,13 +1,24 @@
 """
 """
 
+from SnakeGame.direction import Direction
 import pygame
+
+from position import Position
+from snake import Snake
 
 
 class SnakeGame:
+    GAME_FPS = 60
+
+
     def __init__(self) -> None:
         self._init_pygame()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1024, 768))
+
+        self.board = None
+        self.snake = Snake(Position(10, 10))
 
 
     def main_loop(self):
@@ -31,11 +42,22 @@ class SnakeGame:
             if event.type == pygame.QUIT:
                 quit()
 
+        is_key_pressed = pygame.key.get_pressed()
+        if is_key_pressed[pygame.K_RIGHT]:
+            self.snake.direction = Direction.EAST
+        elif is_key_pressed[pygame.K_LEFT]:
+            self.snake.direction = Direction.WEST
+        elif is_key_pressed[pygame.K_UP]:
+            self.snake.direction = Direction.NORTH
+        elif is_key_pressed[pygame.K_DOWN]:
+            self.snake.direction = Direction.SOUTH
+
 
     def _process_game_logic(self):
-        pass
+        self.snake.move()
 
 
     def _draw(self):
         self.screen.fill(0, 0, 255)
         pygame.display.flip()
+        self.clock.tick(SnakeGame.GAME_FPS)
