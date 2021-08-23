fc04bdd588fd838c40dff2ff2571b04823f7c9ee
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2021-08-23 21:29:58 +0200

    Add an emplty board.py

--- SnakeGame/board.py
+++ SnakeGame/board.py
@@ -1,3 +1,24 @@
 """
+SnakeGame Board
 """
 
+from snake import Snake
+
+class Board:
+    """
+    Instance of this class is used to draw board and all objects on it.
+    Including the snake.
+    """
+    
+    def __init__(self, width: int, height: int, snake: Snake) -> None:
+        self.width = width
+        self.height = height
+        self.snake = snake
+
+
+    def draw(self, screen):
+        # TODO Implement
+        pass
+
+
+    def check_snake_collision(self):
